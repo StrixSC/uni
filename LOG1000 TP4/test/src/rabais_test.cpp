50df64b6f6954665b8fb3d048229917b849b85af
Nawras <strixk9@gmail.com>
2019-03-29 01:36:35 -0400

    modification 1

--- LOG1000 TP4/test/src/rabais_test.cpp
+++ LOG1000 TP4/test/src/rabais_test.cpp
@@ -1,9 +1,7 @@
 #include "rabais_test.h"
 
-#include "diviseur_test.h"
-
 void RabaisTest::setUp() {
-		this->objet_a_tester = new RabaisTest();
+		this->objet_a_tester = new Rabais();
 }
 
 void RabaisTest::tearDown() {
@@ -11,6 +9,18 @@ void RabaisTest::tearDown() {
 }
 
 void RabaisTest::test_rabais_employe(){
-    this->objet_a_tester->getRabais(f, 90001);
-    CPPUNIT_ASSERT_EQUAL(0.2,getRabais);
+        tm date;
+        date.tm_year = 2019;
+        date.tm_mon = 12;
+        date.tm_mday = 13;
+
+        Client* client = new Client(90102, "Simmons", "Marcos", 66, "H4B", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(5.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 90102);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.2, rabais, FLT_EPSILON);
 }
