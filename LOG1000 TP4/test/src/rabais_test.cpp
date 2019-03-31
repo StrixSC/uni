a0c2eed9e2602d9255eee7d6bd09f83a5150e04e
Nawras <strixk9@gmail.com>
2019-03-31 03:06:53 -0400

    modification 4

--- LOG1000 TP4/test/src/rabais_test.cpp
+++ LOG1000 TP4/test/src/rabais_test.cpp
@@ -10,173 +10,202 @@ void RabaisTest::tearDown() {
 
 void RabaisTest::test_rabais_SRS01(){
         tm date;
-        date.tm_year = 2019; 
+        date.tm_year = 2019 - 1900; 
         date.tm_mon = 01;
-        date.tm_mday = 15;
+        date.tm_mday = 01;
 
-        Client* client = new Client(90034, "Travaillant", "Mark", 25, "H1K", date);
+        Client* client = new Client(90001, "Amin", "Nawras", 21, "L0L", date);
         this->objet_a_tester->ajouterClient(client);
 
         Facture facture;
-        facture.ajouterItem(5.0);
+        facture.ajouterItem(1.0);
 
-        double rabais = this->objet_a_tester->getRabais(facture, 90034);
+        double rabais = this->objet_a_tester->getRabais(facture, 90001);
 
         CPPUNIT_ASSERT_DOUBLES_EQUAL(0.2, rabais, FLT_EPSILON);
+        
 }
 
 void RabaisTest::test_rabais_SRS02(){
         tm date;
-        date.tm_year = 2019; 
+        date.tm_year = 2019 - 1900; 
         date.tm_mon = 01;
-        date.tm_mday = 15;
+        date.tm_mday = 01;
 
-        Client* client = new Client(90034, "Travaillant", "Mark", 25, "H1K", date);
+        Client* client = new Client(90001, "Amin", "Nawras", 21, "L0L", date);
         this->objet_a_tester->ajouterClient(client);
 
         Facture facture;
-        facture.ajouterItem(5.0);
+        facture.ajouterItem(1.0);
 
-        double rabais = this->objet_a_tester->getRabais(facture, 90034);
+        double rabais = this->objet_a_tester->getRabais(facture, 90001);
 
         CPPUNIT_ASSERT_DOUBLES_EQUAL(0.2, rabais, FLT_EPSILON);
+        
 }
 
 void RabaisTest::test_rabais_SRS03(){
         tm date;
-        date.tm_year = 2008;
-        date.tm_mon = 12;
-        date.tm_mday = 13; // On set la date d'adhesion du client, ici elle est d'environ 12 ans, ce qui devrait donner un rabais supplementaire de 12/3 = 4% (donc 0.04, mais comme le client est un employe, il n'y devrait pas y avoir de rabais supplementaire)
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
 
-        Client* client = new Client(90102, "Simmons", "Marcos", 66, "H4B", date); //On creer le Client selon le fichier client.dat
+        Client* client = new Client(90002, "Amin", "Nawras", 65, "L0L", date);
         this->objet_a_tester->ajouterClient(client);
 
-        Facture facture; //On creer une facture
-        facture.ajouterItem(500.0); //Ajout de 500 items, qui devrait donner 0.05 sur le rabais, mais comme le client est un employe, il n'y devrait pas y avoir de rabais supplementaire;
+        Facture facture;
+        facture.ajouterItem(1.0);
 
-        double rabais = this->objet_a_tester->getRabais(facture, 90102);
+        double rabais = this->objet_a_tester->getRabais(facture, 90002);
 
         CPPUNIT_ASSERT_DOUBLES_EQUAL(0.2, rabais, FLT_EPSILON);
+        
 }
 
 void RabaisTest::test_rabais_SRS04(){
         tm date;
-        date.tm_year = 2010; 
-        date.tm_mon = 01;
-        date.tm_mday = 30;
-
-        //Creation du client selon un des clients dans le fichier client.dat
-        //Comme le code du client est <90 000, le(la) client(e) n'est pas un(e) employe(e), donc le rabais n'est pas bloque a 0.2.
-        //Ce(Cette) client(e) a + de 65 ans, donc rabais commence a 0.05
-        //Le(La) client(e) est abonne(e) depuis 2010-01-03 (donc 2019-2010 = 9 ans -> 9/3 = 0.03 de plus sur le rabais total)
-        //Le(La) client(e) n'est pas situee dans une des zones: G0X, H4L, H2X, donc aucun rabais supplementaire
-        Client* client = new Client(14770, "Bean", "Paula", 78, "H2H", date);
-        this->objet_a_tester->ajouterClient(client);
-
-        //On creer une facture avec que 5 items dedans, donc aucun rabais supplementaire;
-        Facture facture;
-        facture.ajouterItem(5.0);
-
-        double rabais = this->objet_a_tester->getRabais(facture, 14770);
-        //Le rabais que nous devons avoir (oracle) = 0.08 == rabais_personne_agee + rabais_adhesion
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.08, rabais, FLT_EPSILON);
-}
-
-void RabaisTest::test_rabais_SRS05(){
-        tm date;
-        date.tm_year = 2016; 
-        date.tm_mon = 10;
-        date.tm_mday = 25;
-
-        //Creation du client selon un des clients dans le fichier client.dat
-        //Comme le code du client est <90 000, le(la) client(e) n'est pas un(e) employe(e), donc le rabais n'est pas bloque a 0.2.
-        
-        //Ce(Cette) client(e) a moins de 65 ans, donc aucun rabais supplementaire de personne agee
-        //Le(La) client(e) est abonne(e) depuis 2016-10-25 (donc 2019-2016 = 3 ans -> 3/3 = 0.01 de plus sur le rabais total)
-        //Le(La) client(e) est situe(e) dans une des zones: G0X, H4L, H2X, donc rabais supplementaire de: 0.02 (car G0X)
-        Client* client = new Client(10456, "Tremblay", "Anna", 45, "G0X", date);
-        this->objet_a_tester->ajouterClient(client);
-
-        //On creer une facture avec que 5 items dedans, donc aucun rabais supplementaire;
-        Facture facture;
-        facture.ajouterItem(5.0);
-
-        double rabais = this->objet_a_tester->getRabais(facture, 10456);
-        //Le rabais que nous devons avoir (oracle) = 0.03 == rabais_zone(G0X) + rabais_adhesion
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.03, rabais, FLT_EPSILON);
-}
-
-
-void RabaisTest::test_rabais_SRS05(){
-        tm date;
-        date.tm_year = 2016; 
-        date.tm_mon = 10;
-        date.tm_mday = 25;
-
-        //Creation du client selon un des clients dans le fichier client.dat
-        //Comme le code du client est <90 000, le(la) client(e) n'est pas un(e) employe(e), donc le rabais n'est pas bloque a 0.2.
-        
-        //Ce(Cette) client(e) a moins de 65 ans, donc aucun rabais supplementaire de personne agee
-        //Le(La) client(e) est abonne(e) depuis 2016-10-25 (donc 2019-2016 = 3 ans -> 3/3 = 0.01 de plus sur le rabais total)
-        //Le(La) client(e) est situe(e) dans une des zones: G0X, H4L, H2X, donc rabais supplementaire de: 0.02 (car G0X)
-        Client* client = new Client(10456, "Tremblay", "Anna", 45, "G0X", date);
-        this->objet_a_tester->ajouterClient(client);
-
-        //On creer une facture avec que 5 items dedans, donc aucun rabais supplementaire;
-        Facture facture;
-        facture.ajouterItem(5.0);
-
-        double rabais = this->objet_a_tester->getRabais(facture, 10456);
-        //Le rabais que nous devons avoir (oracle) = 0.03 == rabais_zone(G0X) + rabais_adhesion
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.03, rabais, FLT_EPSILON);
-}
-
-void RabaisTest::test_rabais_SRS05(){
-        tm date;
-        date.tm_year = 2016; 
-        date.tm_mon = 10;
-        date.tm_mday = 25;
-
-        //Creation du client selon un des clients dans le fichier client.dat
-        //Comme le code du client est <90 000, le(la) client(e) n'est pas un(e) employe(e), donc le rabais n'est pas bloque a 0.2.
-        
-        //Ce(Cette) client(e) a moins de 65 ans, donc aucun rabais supplementaire de personne agee
-        //Le(La) client(e) est abonne(e) depuis 2016-10-25 (donc 2019-2016 = 3 ans -> 3/3 = 0.01 de plus sur le rabais total)
-        //Le(La) client(e) est situe(e) dans une des zones: G0X, H4L, H2X, donc rabais supplementaire de: 0.02 (car G0X)
-        Client* client = new Client(10456, "Tremblay", "Anna", 45, "G0X", date);
-        this->objet_a_tester->ajouterClient(client);
-
-        //On creer une facture avec que 5 items dedans, donc aucun rabais supplementaire;
-        Facture facture;
-        facture.ajouterItem(5.0);
-
-        double rabais = this->objet_a_tester->getRabais(facture, 10456);
-        //Le rabais que nous devons avoir (oracle) = 0.03 == rabais_zone(G0X) + rabais_adhesion
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.03, rabais, FLT_EPSILON);
-}
-
-
-void RabaisTest::test_rabais_SRS06(){
-        tm date;
-        date.tm_year = 2011; 
+        date.tm_year = 2019 - 1900; 
         date.tm_mon = 01;
         date.tm_mday = 01;
 
-        //Creation du client selon un des clients dans le fichier client.dat
-        //Comme le code du client est <90 000, le(la) client(e) n'est pas un(e) employe(e), donc le rabais n'est pas bloque a 0.2.
-        
-        //Ce(Cette) client(e) a moins de 65 ans, donc aucun rabais supplementaire de personne agee
-        //Le(La) client(e) est abonne(e) depuis 2016-10-25 (donc 2019-2016 = 3 ans -> 3/3 = 0.01 de plus sur le rabais total)
-        //Le(La) client(e) est situe(e) dans une des zones: G0X, H4L, H2X, donc rabais supplementaire de: 0.02 (car G0X)
-        Client* client = new Client(00001, "Amin", "Nawras", 21ç, "H3L", date);
+        Client* client = new Client(00002, "Amin", "Nawras", 65, "L0L", date);
         this->objet_a_tester->ajouterClient(client);
 
-        //On creer une facture avec que 5 items dedans, donc aucun rabais supplementaire;
         Facture facture;
-        facture.ajouterItem(5.0);
+        facture.ajouterItem(1.0);
 
-        double rabais = this->objet_a_tester->getRabais(facture, 10456);
-        //Le rabais que nous devons avoir (oracle) = 0.03 == rabais_zone(G0X) + rabais_adhesion
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.03, rabais, FLT_EPSILON);
+        double rabais = this->objet_a_tester->getRabais(facture, 00002);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.05, rabais, FLT_EPSILON);
+
 }
 
+
+void RabaisTest::test_rabais_SRS05_1(){
+        tm date;
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "G0X", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(1.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.02, rabais, FLT_EPSILON);
+        
+}
+
+void RabaisTest::test_rabais_SRS05_2(){
+        tm date;
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "H4L", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(1.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.02, rabais, FLT_EPSILON);
+        
+}
+
+void RabaisTest::test_rabais_SRS05_3(){
+        tm date;
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "H2X", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(0.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.01, rabais, FLT_EPSILON);
+        
+}
+
+void RabaisTest::test_rabais_SRS06(){
+        tm date;
+        date.tm_year = 2010 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "L0L", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(1.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.01, rabais, FLT_EPSILON);
+        
+}
+
+void RabaisTest::test_rabais_SRS07(){
+        tm date;
+        date.tm_year = 2000 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "L0L", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(1.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.05, rabais, FLT_EPSILON);
+        
+        
+}
+
+void RabaisTest::test_rabais_SRS08(){
+        tm date;
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "L0L", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(300.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.03, rabais, FLT_EPSILON);
+        
+        
+}
+
+void RabaisTest::test_rabais_SRS09(){
+        tm date;
+        date.tm_year = 2019 - 1900; 
+        date.tm_mon = 01;
+        date.tm_mday = 01;
+
+        Client* client = new Client(00001, "Amin", "Nawras", 21, "L0L", date);
+        this->objet_a_tester->ajouterClient(client);
+
+        Facture facture;
+        facture.ajouterItem(1000.0);
+
+        double rabais = this->objet_a_tester->getRabais(facture, 00001);
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.05, rabais, FLT_EPSILON);
+        
+        
+}
--- LOG1000 TP4/test/src/rabais_test.h
+++ LOG1000 TP4/test/src/rabais_test.h
@@ -11,7 +11,17 @@ using namespace CppUnit;
 class RabaisTest : public CppUnit::TestFixture {
 
     CPPUNIT_TEST_SUITE(RabaisTest);
-    CPPUNIT_TEST(test_rabais_employe);
+    CPPUNIT_TEST(test_rabais_SRS01);
+    CPPUNIT_TEST(test_rabais_SRS02);
+    CPPUNIT_TEST(test_rabais_SRS03);
+    CPPUNIT_TEST(test_rabais_SRS04);
+    CPPUNIT_TEST(test_rabais_SRS05_1);
+    CPPUNIT_TEST(test_rabais_SRS05_2);
+    CPPUNIT_TEST(test_rabais_SRS05_3);
+    CPPUNIT_TEST(test_rabais_SRS06);
+    CPPUNIT_TEST(test_rabais_SRS07);
+    CPPUNIT_TEST(test_rabais_SRS08);
+    CPPUNIT_TEST(test_rabais_SRS09);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -23,7 +33,17 @@ public:
     void tearDown();
 
     //Fonctions de tests
-    void test_rabais_employe();
+    void test_rabais_SRS01();
+    void test_rabais_SRS02();
+    void test_rabais_SRS03();
+    void test_rabais_SRS04();
+    void test_rabais_SRS05_1();
+    void test_rabais_SRS05_2();
+    void test_rabais_SRS05_3();
+    void test_rabais_SRS06();
+    void test_rabais_SRS07();
+    void test_rabais_SRS08();
+    void test_rabais_SRS09();
 };
 
 #endif
