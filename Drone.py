6e9c077bfb3c098c7998eb4bb64ba7833496d29f
Nawras Amin <43833100+NawrasAmin@users.noreply.github.com>
2019-10-28 15:55:57 -0400

    Nawras: Update Drone.py
    Petit update pendant que javais 5 minutes de pause :)

--- Drone.py
+++ Drone.py
@@ -1,3 +1,23 @@
-class Drone(object):
-    __init__(self, type):
-        self.type = type
+from graph import Graph
+
+class Drone:
+    
+    #Les variables necessaires pour le calcul de distance du robot
+    typeRobot = ''
+    k = 0
+    m = 0
+    graph = Graph(#parametres a specifie)
+
+    __init__(self, typeRobot):
+        self.typeRobot = typeRobot
+        
+    def prendreCommande:
+        
+        if typeRobot == X:
+            k = (1 + m)
+        elif typeRobot == Y:
+            k = (1.5 + (0.6*m))
+        elif typeRobot == Z: 
+            k = (2.5 + (0.2*m))
+            
+        
