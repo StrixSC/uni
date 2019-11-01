b1c1fa82660293a662bc14552478cba6c929fc6c
Ragib <ahashanragib@gmail.com>
2019-11-01 15:50:27 -0400

    lolz

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -91,49 +91,60 @@ def dijkstra_algorithm(graph, starting_node):
 
 
 def dijkstra(graph, departure_node):
-	list_nodes = []
-
-	for node in graph.list_vertex[:]:
-		list_nodes.append([node , 1000000])
-
-
-	list_nodes[int(departure_node.id)][1] = 0
-
-
-	list_nodes = np.array(list_nodes)
-	#print(list_nodes)
-
-	shortest_distances = [ 1000000 for i in range(len(graph.list_vertex))]
-	shortest_distances[int(departure_node.id)] = 0
-	previous_nodes 	   = [ 'unknown' for i in range(len(graph.list_vertex))]
-
-	current_shortest = 1000000
 	
-	visiting_node = 0
 
-	for i in range(25):
+	#[ VertexObject (Class:Vertex), Visited (Type:Boolean), shortest_distance (type:int), previous_node (type:string)  ]
+	# computational_matrix_data is a 4x21 matrix, and it contains all the data necessary for computation of the algorithm.
 
-		visiting_node = departure_node
+	computational_matrix_data = [[graph.list_vertex[j], False, 1000000, 'unknown'] for j in range(21)]
 
-		for node in list_nodes:
-			if node[1] < current_shortest and node[0] != 'visited':
-				visiting_node = node[0]
-				print(visiting_node.id)
-				list_nodes[0][0] = 'visited'
-
-
-		print("asdad: ", visiting_node.id)
+	# Index guide for the matrix list_nodes:
+	vertex_index 			= 0
+	visited_index 			= 1
+	shortest_distance_index = 2
+	previous_node_index 	= 3
 
 
-		for neighbor in visiting_node.get_neighbors_distances():
-			total_distance = list_nodes[int(visiting_node.id)][1] + neighbor[1]
-			if( total_distance < list_nodes[int(neighbor[0].id)][1]):
-				list_nodes[int(neighbor[0].id)][1] = total_distance
+	# Doesn't affect the inner logic, but beautifies the prints of the matrix. Which makes it more understandable.
+	computational_matrix_data = np.array(computational_matrix_data)
+
+	# The shortest distance for the source node must be 0. Since, it's already there!
+	computational_matrix_data[int(departure_node.id)][shortest_distance_index]  = 0
+	computational_matrix_data[int(departure_node.id)][previous_node_index] 		= 'none'
+
+
+	#print("printing computational_matrix_data matrix: \n ", computational_matrix_data , "\n")
+	shortest_dist_path = 1000000
+	current_vertex 	   = departure_node
+
+
+	for k in range(7):
+		shortest_dist_path = 1000000
+		for node in computational_matrix_data:
+			if (node[shortest_distance_index] < shortest_dist_path) and (node[visited_index] == False):
+				shortest_dist_path  = node[shortest_distance_index]
+				path_vertex	    = node[vertex_index]
+				node[visited_index] = True
+		
+		print("\n shortest_dist_path chosen: ", shortest_dist_path)
+		print("Visiting the vertex: ", path_vertex.id)
+
+		for neighbor in path_vertex.get_neighbors_distances():
+			# neighbor[0] : returns an object Vertex. Returns a neighbor our current vertex 
+			# neighbor[1] : returns the distance of the arc between our current vertex and it's neighbor
+			neighbor_distance_from_source = shortest_dist_path + neighbor[1]
+			current_distance_neighbor_source = computational_matrix_data[int(neighbor[0].id)][shortest_distance_index]
+			
+			# If the new distance calculated with our path is less than it's know distance, update the distance.
+			if(neighbor_distance_from_source < current_distance_neighbor_source) and (computational_matrix_data[int(neighbor[0].id)][visited_index] == False):
+				computational_matrix_data[int(neighbor[0].id)][shortest_distance_index] = neighbor_distance_from_source
+				computational_matrix_data[int(neighbor[0].id)][previous_node_index]		= str(path_vertex.id)
+
+		
 
 
 
-	print(list_nodes)
-
+		print("printing computational_matrix_data matrix: ", k , "\n", computational_matrix_data , "\n")
 
 
 
@@ -144,6 +155,11 @@ def main():
 
 	graph = Graph(list_vertices, list_arcs)
 	#graph.printGraph()
+
+	list_mat = [[list_vertices[j], False, 1000000, 'unknown'] for j in range(21)]
+	list_mat = np.array(list_mat)
+	
+
 
 
 	#dijkstra_algorithm(graph, graph.list_vertex[0])
