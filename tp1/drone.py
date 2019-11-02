d851533cc7b8debd15e5ffe008654885f5eb47dd
Ragib <ahashanragib@gmail.com>
2019-11-01 21:00:28 -0400

    Drone class done

---
+++ tp1/drone.py
@@ -0,0 +1,49 @@
+class Drone:
+	def __init__(self, typeDrone, mass = 0):
+		# Error handling.
+		self.correct_drone_choice = True
+		self.typeDrone  = typeDrone			
+		
+		# 
+		if typeDrone == 'X':
+			if(mass > 5):
+				self.correct_drone_choice = False
+				print("ERROR: Drone X cannot hold a mass of ", mass, "kg! Please change drone type. \n ")
+			else:
+				self.maxCharge  = 5
+				self.constant_K = 1 + mass
+				self.carry_mass = mass
+
+		elif typeDrone == 'Y':
+			if(mass > 10):
+				self.correct_drone_choice = False
+				print("ERROR: Drone Y cannot hold a mass of ", mass, "kg! Please change drone type. \n ")
+			else:
+				self.maxCharge = 10
+				self.constant_K = 1.5 + 0.6*mass
+				self.carry_mass = mass
+
+		elif typeDrone == 'Z':
+			if(mass > 25):
+				self.correct_drone_choice = False
+				print("ERROR: Drone Z cannot hold a mass of ", mass, "kg! Please change drone type. \n ")
+			else:
+				self.maxCharge = 25
+				self.constant_K = 2.5 + 0.2*mass
+				self.carry_mass = mass
+		else:
+			self.correct_drone_choice = False
+			print("\n ERROR: TYPE OF DRONE ", typeDrone, "DOES NOT EXIST! \n")
+
+		if self.correct_drone_choice == True:
+			print("Drone Type: ", self.typeDrone, "   maxCharge: ", self.maxCharge, "   Constant K: ", self.constant_K)
+
+
+	def change_mass(self, mass):
+		self.carry_mass = mass
+
+	def printDrone(self):
+		if self.correct_drone_choice == True:
+			print("Drone Type: ", self.typeDrone, "   maximum load capacity: ", self.maxCharge, "   Constant K: ", self.constant_K, ". It is/will carry", self.carry_mass, "kg for the current/next mission.")
+		else:
+			print("----- ERROR: THIS DRONE CANNOT BE USED OR DOESN'T EXIST! ----")
--- tp1/tp1.py
+++ tp1/tp1.py
@@ -7,12 +7,13 @@
 
 
 # Le format représenté dans le fichier externe entrepot.txt est le format d'un CSV file.
-
 import csv
 from Arc import Arc
 from Vertex import Vertex
 from commande import Commande
 from Graph import Graph
+from Dijkstra import printPaths, compute_fastest_paths_dijstra
+from drone import Drone
 import numpy as np
 
 
@@ -50,97 +51,12 @@ def read_file(file_name = 'entrepot.txt'):
 
 
 
-
-def printPaths(computational_matrix_data, node_id, source_id ):
-	
-	vertex_index 			= 0
-	visited_index 			= 1
-	shortest_distance_index = 2
-	previous_node_index 	= 3
-	if computational_matrix_data[node_id][previous_node_index] == str(source_id):
-		return  str(source_id) + '->' + str(node_id)
-	else:
-		prev_node = computational_matrix_data[node_id][previous_node_index]	
-		return printPaths(computational_matrix_data, int(prev_node), source_id) + "->" + str(node_id)
-
-
-def compute_fastest_paths_dijstra(graph, departure_node):
-	
-
-	#[ VertexObject (Class:Vertex), Visited (Type:Boolean), shortest_distance (type:int), previous_node (type:string)  ]
-	# computational_matrix_data is a 4x21 matrix, and it contains all the data necessary for computation of the algorithm.
-
-	computational_matrix_data = [[graph.list_vertex[j], False, 1000000, ''] for j in range(len(graph.list_vertex))]
-
-	# Index guide for the matrix list_nodes:
-	vertex_index 			= 0
-	visited_index 			= 1
-	shortest_distance_index = 2
-	previous_node_index 	= 3
-	# computational_matrix_data[vertex_index][visited_index][shortest_distance_index][previous_node_index]
-
-
-	# Doesn't affect the inner logic, but beautifies the prints of the matrix. Which makes it more understandable.
-	computational_matrix_data = np.array(computational_matrix_data)
-
-	# The shortest distance for the source node must be 0. Since, it's already there!
-	computational_matrix_data[int(departure_node.id)][shortest_distance_index]  = 0
-	computational_matrix_data[int(departure_node.id)][previous_node_index] 		= '0'
-
-
-	shortest_dist_path = 1000000
-	current_vertex 	   = departure_node
-
-
-	for k in range(len(computational_matrix_data)):
-		shortest_dist_path = 1000000
-		for node in computational_matrix_data:
-			if (node[shortest_distance_index] < shortest_dist_path) and (node[visited_index] == False):
-				shortest_dist_path  = node[shortest_distance_index]
-				path_vertex	    = node[vertex_index]
-		
-		computational_matrix_data[int(path_vertex.id)][visited_index] = True
-		
-	
-		for neighbor in path_vertex.get_neighbors_distances():
-			# neighbor[0] : returns an object Vertex. Returns a neighbor our current vertex. See Vertex class in Vertex.py for more info.
-			# neighbor[1] : returns the distance of the arc between our current vertex and it's neighbor. See Vertex class in Vertex.py for more info.
-			neighbor_distance_from_source = shortest_dist_path + neighbor[1]
-			current_distance_neighbor_source = computational_matrix_data[int(neighbor[0].id)][shortest_distance_index]
-			
-			# If the new distance calculated with our path is less than it's know distance, update the distance.
-			if(neighbor_distance_from_source < current_distance_neighbor_source) and (computational_matrix_data[int(neighbor[0].id)][visited_index] == False):
-				computational_matrix_data[int(neighbor[0].id)][shortest_distance_index] = neighbor_distance_from_source
-				computational_matrix_data[int(neighbor[0].id)][previous_node_index]		= str(path_vertex.id)
-
-
-
-	
-	
-	nodes 				 = np.array([ [computational_matrix_data[i][vertex_index]] for i in range(len(computational_matrix_data))])
-	shortest_distances   = np.array([[computational_matrix_data[i][shortest_distance_index]] for i in range(len(computational_matrix_data))])
-	fastest_paths        = np.array([[printPaths(computational_matrix_data, i, int(departure_node.id))] for i in range(len(computational_matrix_data))])
-	fastest_paths_matrix = np.array([ [nodes[i], shortest_distances[i], fastest_paths[i]] for i in range(len(computational_matrix_data))])
-
-	# fastest_paths_matrix is a 3x21 matrix in the scope of our assignment.
-	# the first  column contains all the vertices. 									[Object type: Vertex. (See Vertex class in Vertex.py file)]
-	# the second column contains all the shortest distances for the i-th vertex 	[Object type: integer]
-	# the third  column contains the shortest path. From departure to destination.	[Object type: string]
-	return fastest_paths_matrix
-
-
-
-
-
-
-
-
-
-
 def main():
 	list_vertices, list_arcs = read_file()
 
 	graph = Graph(list_vertices, list_arcs)
+	#path = PathFinder(graph, list_vertices[0])
+	#fast = path.compute_fastest_paths_dijstra(graph, list_vertices[0])
 	#graph.printGraph()
 
 	
@@ -150,8 +66,10 @@ def main():
 
 	fastest_paths = compute_fastest_paths_dijstra(graph, graph.list_vertex[10])
 	
-	print(fastest_paths)
-
+	droneX = Drone('X', mass = 5)
+	droneX.printDrone()
+	#droneY = Drone('Y', mass = 10)
+	#droneZ = Drone('Z', mass = 25)
 
 	
 
