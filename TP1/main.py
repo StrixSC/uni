f99580aa35deadff9529d4ef30edc453f20c580f
Nawras Mohammed Amin <namohc@l4714-01.info.polymtl.ca>
2022-02-23 21:24:33 -0500

    Added 'ms' to the print statement in the end

--- TP1/main.py
+++ TP1/main.py
@@ -47,7 +47,7 @@ def main():
     solved, time = skyline.solve(algorithm=args.algo, p=args.p, seuil=100)
     
     if args.t:
-        print("Temps: ", time * 1000)
+        print("Temps: ", time * 1000, "ms")
     
     if args.p:
         for cp in solved:
