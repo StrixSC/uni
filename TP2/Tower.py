107ccd302ac219b8c303f6fc4ce4abc5bb4a79fa
StrixSC <talon.strix@gmail.com>
2022-02-26 12:49:06 -0500

    Updated gitignore and fixed typos in Tower.py

--- TP2/Tower.py
+++ TP2/Tower.py
@@ -9,7 +9,7 @@ class Tower:
         elif algorithm == "progdyn":
             solved, height = self.solve_dynamic_programming(blocks)
         elif algorithm == "tabou":
-            solved, height = self.solve_tabou(blocks)
+            solved, height = self.solve_tabu(blocks)
         else:
             print("Algorithm pas implémenté")
             exit(-1)
@@ -24,7 +24,7 @@ class Tower:
         # On pose le premier block de la liste comme fondation de la tour. 
         # On pose la hauteur de se dernier comme la hauteur initiale de la tour.
         tower.append(blocks[0])
-        tower_height = tower[-1].h
+        hauteur = tower[-1].h
 
         # Pour chaque block restant, on verifie si la longueur et la profondeur sont strictement inférieur à celle du dernier block ajouté à la tour.    
         for i in range(1, len(blocks)):
@@ -32,12 +32,17 @@ class Tower:
             if block.l < tower[-1].l and block.p < tower[-1].p:
                 # Si oui, on l'ajoute à la tour et on augmente la hauteur totale de la tour.
                 tower.append(block)
-                tower_height += block.h
+                hauteur += block.h
 
-        return tower, tower_height
+        return tower, hauteur
         
     def solve_dynamic_programming(self, blocks: list[Block]):
+        # Tout d'abord, on trie la liste de block selon l'aire des blocs en ordre decroissant
+        blocks.sort(key=lambda x: x.l*x.p) 
+        hauteur = 0
+        
+        
         return []
     
-    def solve_tabou(self, blocks: list[Block]):
+    def solve_tabu(self, blocks: list[Block]):
         return []
