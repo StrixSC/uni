9c60b3021cf888d5098ef1a755f32c8a5f865c1c
StrixSC <talon.strix@gmail.com>
2022-04-03 01:35:50 -0400

    started greedy solve, but turns out its useless after re-reading the prompt :')

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -2,15 +2,15 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <iterator>
-#include <sstream>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-void print_adjacency_matrix(const vector<vector<int>>& graph)
+void print_matrix(const vector<vector<int>>& matrix)
 {
     unsigned i = 0;
-    for (auto v1 : graph)
+    for (auto v1 : matrix)
     {
         printf("%d [ ", i);
         for (auto v2 : v1)
@@ -18,6 +18,68 @@ void print_adjacency_matrix(const vector<vector<int>>& graph)
         i++;
         printf("] \n");
     }
+}
+
+vector<pair<int, int>> get_link_counts(const vector<vector<int>>& graph)
+{
+    vector<pair<int, int>> link_counts;
+    unsigned int index = 0;
+    for(auto v : graph)
+    {
+        unsigned int count = 0;
+        for (auto itr = v.begin(); itr != v.end(); itr++)
+        {
+            if(*itr) count++;
+        }
+        link_counts.push_back(make_pair(index, count));
+        index++;
+    }
+    
+    sort(link_counts.begin(), link_counts.end(), 
+        [&](pair<int, int>& a, pair<int, int>& b)
+        {
+            return a.second > b.second;
+        }
+    );
+
+    return link_counts;
+}
+
+vector<pair<int, int>> get_atom_type_energy_sums(const vector<vector<int>>& energy_matrix)
+{
+    vector<pair<int, int>> energy_bond_sum(energy_matrix.size());
+    unsigned int index = 0;
+    for(auto v : energy_matrix)
+    {
+        energy_bond_sum[index].first = index;
+        for (auto itr = v.begin(); itr != v.end(); itr++)
+        {
+            energy_bond_sum[index].second += *itr;
+        }
+        index++;
+    }
+
+    sort(energy_bond_sum.begin(), energy_bond_sum.end(), 
+        [&](pair<int, int>& a, pair<int, int>& b)
+        {
+            return a.second < b.second;
+        }
+    );
+    return energy_bond_sum;
+}
+
+// EDIT: THIS WILL NOT WORK OUT XDD
+size_t solve_greedy(
+    unsigned int site_count,
+    unsigned int type_count,
+    const vector<int> atom_type_counts,
+    const vector<vector<int>>& graph, 
+    const vector<vector<int>>& energy_matrix
+)
+{
+    vector<pair<int, int>> link_counts = get_link_counts(graph);    // O(n^2)
+    vector<pair<int, int>> atom_type_energy_sums = get_atom_type_energy_sums(energy_matrix);    // O(n^2)
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -65,10 +127,9 @@ int main(int argc, char* argv[]) {
         atom_counts.push_back(atom_count);
     }
 
-    // Create a 2D array to contain the bond energies between each site.
+    // Initialize and fill a matrix containing the bond energies between each site.
     vector<vector<int>> bond_energies_matrix(types_count, vector<int>(0));
-    
-    // Fill the bond_energies array.
+
     int energy;
     for(int i = 0; i < types_count; i++)
     {
@@ -87,6 +148,8 @@ int main(int argc, char* argv[]) {
         file >> vertex_a >> vertex_b;
         graph[vertex_a][vertex_b] = 1;
     }
+
+    const size_t sum = solve_greedy(site_count, types_count, atom_counts, graph, bond_energies_matrix);
 
     return 0;
 }
