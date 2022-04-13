9e5932622637cf2d17609ac58384c7f435ce80a2
StrixSC <talon.strix@gmail.com>
2022-04-13 00:08:00 -0400

    full random attempt

--- TP3/main2.cpp
+++ TP3/main2.cpp
@@ -13,53 +13,51 @@
 
 using namespace std;
 
-bool operator<(Node& n1, Node& n2) {
-    return n1.total_cost < n2.total_cost;
-}
-
-Node* solve_bnb(int site_count, vector<vector<int>>& cost_matrix, unordered_map<int, vector<int>>& link_graph, vector<int> type_amounts, int type_count)
+bool same_solution(vector<pair<int, int>>& v1, vector<pair<int, int>>& v2)
 {
-    int min = INT64_MAX;
-
-    Node* next_node = new Node(site_count); // Create root node
-
-    for (int site = 0; site < site_count; site++)
-    {
-        priority_queue<Node*, vector<Node*>> nodes;
-        for (int type = 0; type < type_count; type++)
-        {
-            Node* node = new Node(next_node->state, site, type);
-            node->check_validity(type_amounts, type_count);
-            node->compute_cost(cost_matrix, link_graph);
-            if (node->is_valid)
-            {
-                nodes.push(node);
-            }
-            else
-            {
-                delete node;
-            }
-        }
-
-        if (nodes.top())
-        {
-            delete next_node;
-            next_node = nodes.top();
-            nodes.pop();
-        }
-
-        while (!nodes.empty())
-        {
-            Node* node = nodes.top();
-            nodes.pop();
-            delete node;
-        }
-    }
-
-    return next_node;
+    return equal(v1.begin(), v1.end(), v2.begin());
 }
 
-int main(int argc, char* argv[]) {
+vector<vector<pair<int, int>>>& create_neighbours(vector<int>& solution, int site, vector<int>& connected_vertices)
+{
+    vector<vector<int>> neighbours;
+    for (auto& connected_vertex : connected_vertices)
+    {
+        if (connected_vertex < site)
+        {
+            continue;
+        }
+
+        // Create a new solution, where we'll swap the value for a different value.
+        vector<int> neighbour((int)solution.size(), make_pair(0, 0));
+        copy(solution.begin(), solution.end(), neighbour.begin());
+        int tmp = neighbour[site];
+        neighbour[site] = neighbour[connected_vertex];
+        neighbour[connected_vertex] = tmp;
+        neighbours.push_back(neighbour);
+    }
+    return neighbours;
+}
+
+int compute_cost(vector<int>& solution, vector<vector<int>>& cost_matrix, unordered_map<int, vector<int>>& graph)
+{
+    int total_cost = 0;
+    int index = 0;
+    for (auto& it : graph)
+    {
+        int vertex = it.first;
+        vector<int> connected_vertices = it.second;
+        for (auto connected_vertex : connected_vertices)
+        {
+            total_cost += cost_matrix[solution[vertex]][solution[connected_vertex]];
+            index++;
+        }
+    };
+    return total_cost;
+}
+
+int main(int argc, char* argv[])
+{
 
     if (argc == 1)
     {
@@ -96,8 +94,7 @@ int main(int argc, char* argv[]) {
     vector<int> type_counts;
     int count;
 
-
-    // Get the count of atoms per type 
+    // Get the count of atoms per type
     for (int i = 0; i < type_count; i++)
     {
         file >> count;
@@ -126,44 +123,55 @@ int main(int argc, char* argv[]) {
         graph[vertex_a].push_back(vertex_b);
     }
 
-    unordered_map<int, int> random_solution;
+
+    vector<int> best_solution;
     vector<int> all_types(type_count, 0);
     copy(type_counts.begin(), type_counts.end(), all_types.begin());
     for (int site = 0; site < site_count; site++)
     {
-        int random_type = 0;
-        do {
-            random_type = floor((rand() % type_count));
-        } while (all_types[random_type] == 0);
-        random_solution.insert(make_pair(site, (int) random_type));
-        all_types[random_type]--;
-    }
-
-    Node* best = new Node(random_solution);
-    best->compute_cost(cost_matrix, graph);
-    best->check_validity(type_counts, type_count);
-
-    while (true)
-    {
-        for (auto& it : graph)
+        for (int i = 0; i < type_count; i++)
         {
-            vector<Node*> neighbours = best->create_neighbours(it.first, it.second);
-            for (auto n : neighbours)
+            if (all_types[i] > 0)
             {
-                n->compute_cost(cost_matrix, graph);
-                if (n->total_cost < best->total_cost)
-                {
-                    delete best;
-                    best = n;
-                    cout << best->get_solution() << endl;
-                    cout << best->total_cost << endl;
-                }
-                else
-                {
-                    delete n;
-                }
+                best_solution.push_back(i);
+                all_types[i]--;
+                break;
             }
         }
+    }
+    auto rng = std::default_random_engine {};
+    int best_cost = compute_cost(best_solution, cost_matrix, graph);
+    while (true)
+    {
+        vector<int> shuffled(site_count, 0);
+        copy(best_solution.begin(), best_solution.end(), shuffled.begin());
+        shuffle(shuffled.begin(), shuffled.end(), rng);
+        int cost = compute_cost(shuffled, cost_matrix, graph);
+        if (cost < best_cost)
+        {
+            best_solution = shuffled;
+            best_cost = cost;
+            // copy(best_solution.begin(), best_solution.end(), ostream_iterator<int>(cout, " "));
+            cout << cost << endl;
+        }
+        // for (auto& it : graph)
+        // {
+        //     vector<vector<pair<int, int>>> neighbours = create_neighbours(best_solution, it.first, it.second);
+        //     for (auto n : neighbours)
+        //     {
+        //         int cost = compute_cost(n, cost_matrix, graph);
+        //         if (cost < best_cost)
+        //         {
+        //             best_solution = n;
+        //             for (auto& v : best_solution)
+        //             {
+        //                 cout << v.first << " - " << v.second << endl;
+        //             }
+        //             cout << cost << endl;
+        //             best_cost = cost;
+        //         }
+        //     }
+        // }
     }
 
     return 0;
