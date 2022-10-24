624abc3f32533b3ed88b19b82baeb20f8ca02f6b
StrixSC <talon.strix@gmail.com>
2022-10-24 15:30:55 -0400

    Added forces computation functions

--- Devoir2/Devoir2.m
+++ Devoir2/Devoir2.m
@@ -6,14 +6,35 @@
 function [Vf t x y z]=Devoir2(pos, ar, va, lambda)
 
 % Definitions of constants
-rocket_weight = 320; % in tonnes
-fuel_weight = 270; % in tonnes
-rocket_cone_radius = rocket_cylinder_radius = 1.8; % in meters
-rocket_height= 53; % in meters
-rocket_fuel_consumption = 3.5; % in km/s
+rocket_mass = 320; % in tonnes
+fuel_mass = 270; % in tonnes
+rocket_radius = rocket_cone_radius = rocket_cylinder_radius = 1.8/1000; % in km
+rocket_height= 53/1000; % in km
 earth_radius = 6371; % in km
+jets_position_x = 0;
+jets_position_y = 0;
+jets_position_z = 0 - 25/1000;
 
-% Definition of return values
+% Propulsion constants:
+mu = 1200 % in kg/s
+initial_v_gas = 3.5; % in km/s
+
+% Gravitationnal Constants:
+G = 6.673 * 10^(-1 * 11); % in Nm^2/kg^2
+M_t = 5.974 * 10^24; % in kg
+
+% Friction constants:
+C_vis = 0.5;
+initial_rho = 1.275 % in kg/m^3
+initial_h = 7200 % in m;
+
+
+% Initial Rocket Values: 
+  rocket_initial_velocity = [0; 0; 0]
+  rocket_com = [0; 0; earth_radius + rocket_height/2]; % Global referential
+  rocket_initial_acceleration = 0;
+
+% Definition of final values
   Vf= [0, 0, 0]
   t = [0, 0, 0]
   x = [0, 0, 0]
@@ -21,17 +42,26 @@ earth_radius = 6371; % in km
   z = [0, 0, 0]
 
   function [is_completed] = verify_completion(rocket_com)
-    first = rocket_com(1)^2 + rocket_com(2)^2 + rocket_com(3)^2 >= 10^14;
-    second = rocket_com(1)^2 + rocket_com(2)^2 + rocket_com(3)^2 <= earth_radius^2;
-    is_completed = first || second;
+    d = rocket_com(1)^2 + rocket_com(2)^2 + rocket_com(3)^2;
+    is_completed =  d >= 10^14 || d <= earth_radius^2;
   end
   
-  rocket_com_x = 0;
-  rocket_com_y = 0;
-  rocket_com_z = 0;
+  function [Fp] = compute_propulsion(incline_angle)
+    v_gas = -1 * abs(initial_v_gas) * [0 ; sin(incline_angle) ; cos(incline_angle)];
+    Fp  = -1 * mu * v_gas;
+  end 
 
-  while !verify_completion([rocket_com_x ; rocket_com_y ; rocket_com_z])
-    rocket_com_x = 10^14; % Temporary condition to break while loop
+  function [Fg] = compute_gravitational(rocket_com)
+    g = -1 * G * M_t * (r / (norm(rocket_com)^3))
+    Fg = rocket_mass * g;
   end
 
+  function [Fvis] = compute_friction(alpha)
+    A = (pi * rocket_radius^2 * cos(alpha)) + (2 * rocket_radius * rocket_height * sin(alpha));
+    p = initial_rho*exp(1)^(earth_radius)
+  end
+
+  %%% Uncomment the following three lines to start the simulation: 
+  % while !verify_completion([rocket_com_x ; rocket_com_y ; rocket_com_z])
+  % end
 end
