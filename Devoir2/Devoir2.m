be01cb9b4ba29a8f859e21708b7bc93782ab8151
StrixSC <talon.strix@gmail.com>
2022-10-31 19:54:03 -0400

    Added missing stuff stated by prof

--- Devoir2/Devoir2.m
+++ Devoir2/Devoir2.m
@@ -5,8 +5,116 @@
 
 function [Vf t x y z] = Devoir2(theta)
 
+    function [alpha] = compute_alpha(v, thX)
+        if (norm(v))
+            alpha = acos(v(3)/(norm(v)));
+        else
+            alpha = 0; % The alpha will be parallel to the Z axis
+        end
+    end
+
+    function [is_completed] = verify_completion(r)
+        completed = false;
+        d = r(1)^2 + r(2)^2 + r(3)^2;
+
+        if(d >= 10^14)
+            printf("[!] Rocket out of sight...\n");
+        elseif(d <= earth_radius^2)
+            printf("[!] Crash...\n");
+        end
+
+        is_completed = d >= 10^14 || d <= earth_radius^2;
+    end
+
+    % Rotation matrix about the X axis, at thX angle.
+    function [Rx] = R(thX)
+        Rx = [
+            1, 0, 0; 
+            0, cos(thX), -sin(thX); 
+            0 sin(thX) cos(thX)
+        ];
+    end
+
+    function [mass] = compute_rocket_mass_at_time(time)
+        mass = (rocket_mass + fuel_mass) - 1200 * time;
+        if (mass <= rocket_mass - fuel_mass)
+            mass = rocket_mass;
+        end
+    end
+
+    function [I] = compute_moment_of_inertia(mass)
+        I_zz = mass / 2 * rocket_radius^2;
+        I_xx = I_yy = (mass / 4 * rocket_radius^2) + (mass / 12 * rocket_height^2);
+        I = [
+            I_xx, 0, 0;
+            0, I_yy, 0;
+            0, 0, I_zz;
+            ];
+    end
+
+    function [Fp] = compute_propulsion(r, thX, mass)
+        v_gas = -1 * initial_v_gas * [0; sin(theta); cos(theta)];
+        Fp = -1 * mu * v_gas;
+        if (mass <= rocket_mass)
+            Fp = [0; 0; 0];   
+        else
+            Fp = R(thX)*Fp;
+        end
+    end
+
+    function [Fg] = compute_gravitational(r, mass)
+        g = -1 * G * M_t * (r / (norm(r)^3));
+        Fg = mass * g;
+    end
+
+    function [Fvis] = compute_friction(r, v, alpha)
+        % Add cos alpha absolute values
+        A = (pi * rocket_radius^2 * cos(alpha)) + (2 * rocket_radius * rocket_height * sin(alpha));
+        rho = rho_0 * exp((earth_radius - norm(r)) / h_0);
+        Fvis = -1 * 1/2 * A * rho * C_vis * norm(v) * v;
+    end
+    
+    function [ang_acc] = compute_angular_acceleration(q, t)
+        r = [0; q(3); q(4)];
+        thX = q(6);
+        mass = compute_rocket_mass_at_time(t);
+        propulsion_force = compute_propulsion(r, thX, mass);
+        tau = cross(r_oxyz, propulsion_force); 
+        I_local = compute_moment_of_inertia(mass);
+        Rx = R(thX);
+        MI = Rx * I * transpose(Rx);
+        ang_acc = inv(MI) * tau;
+    end
+
+    function [lin_acc] = compute_linear_acceleration(q, t)
+        v = [0; q(1); q(2)];
+        r = [0; q(3); q(4)];
+        wx = q(5);
+        thX = q(6);
+        mass = compute_rocket_mass_at_time(t);
+        alpha = compute_alpha(v, thX);
+        if (mass <= rocket_mass)
+            Fp = [0; 0; 0];   
+        else
+            Fp = compute_propulsion(r, thX, mass);
+        end
+
+        Fg = compute_gravitational(r, mass);
+        Fvis = compute_friction(r, v, alpha);
+        F = Fp + Fg + Fvis;
+        lin_acc = F / mass;
+    end
+    
+
+    function [g] = compute_g(q, t)
+        lin_acc = compute_linear_acceleration(q, t);
+        ang_acc = compute_angular_acceleration(q, t);
+        %   [  acc_y;      acc_z ; vit_y; vit_z; ang_acc_x; thX ]
+        g = [lin_acc(2), lin_acc(3), q(1), q(2), ang_acc(1), q(6)];
+    end
+    
     % Definitions of constants
-    rocket_mass = 320 * 1000; % in kg
+    rocket_mass = 20 * 1000; % in kg
     fuel_mass = 300 * 1000; % in kg
     rocket_radius = rocket_cone_radius = rocket_cylinder_radius = 1.8; % in meters
     rocket_height = 53; % in meters
@@ -14,13 +122,7 @@ function [Vf t x y z] = Devoir2(theta)
     jets_position_x = 0;
     jets_position_y = 0;
     jets_position_z = 25; % in meters
-
-    % Tiré du document de référence. 
-    R = [
-        cos(theta), -sin(theta),0;
-        sin(theta), cos(theta), 0;
-        0,0,1;
-      ];
+    r_oxyz = [0; 0; -1 * jets_position_z]; % center of mass of the rocket with respect to its own referential.
 
     % Propulsion constants:
     mu = 1200; % in kg/s
@@ -31,116 +133,67 @@ function [Vf t x y z] = Devoir2(theta)
     M_t = 5.974 * 10^24; % in kg
 
     % Friction variables and constants:
-    alpha = 30; % TODO
+    alpha = 0;
     C_vis = 0.5;
     rho_0 = 1.275; % in kg/m^3
     h_0 = 7200; % in m;
 
     % Initial Rocket Values:
-    r = [0; 0; earth_radius + jets_position_z]; % Rocket's center of mass w.r. to global referential in meters
-
+    r0 = [0; 0; earth_radius + jets_position_z]; % Rocket's center of mass w.r. to global referential in meters
+    v0 = [0; 0; 0]; % Rocket's initial velocity. (Rocket is stationary at time 0 ergo the velocity is 0)
+    w0x = 0; % Rocket's initial angular velocity with respect to the X axis. (Rocket is stationary at time 0 ergo the angular velocity is 0)
+    thX = 0; % Rocket's initial angular displacement with respect to the X axis (Rocket is sitting still and thus has not rotated at moment 0).
+    q = [v0(2), v0(3), r0(2), r0(3), w0x, thX];
+    mass0 = rocket_mass;
+    
     % Definition of final values
     t = [0];
-    x = [r(1)];
-    y = [r(2)];
-    z = [r(3)];
+    x = [r0(1)];
+    y = [r0(2)];
+    z = [r0(3)];
+    
+    completed = false;
+    % Starting positions:
+    dT = 0.005; % Set delta t to an arbitrairy value;
+    snapshot_timer = 0.010;
+    printf("[*] Using deltaT: %f and snapshot's timer: %f\n", dT, snapshot_timer);
 
-    function [is_completed] = verify_completion(counter, rocket_CoM)
-        d = rocket_CoM(1)^2 + rocket_CoM(2)^2 + rocket_CoM(3)^2;
-        is_completed = d >= 10^14 || d <= earth_radius^2;
-    end
+    time = 0;
+    time_since_last_snapshot = 0;
 
-    function [Fp] = compute_propulsion(theta)
-        v_gas = -1 * initial_v_gas * [0; sin(theta); cos(theta)];
-        Fp = -1 * mu * v_gas; % do * 1000 to convert to kg*m/s^2 (Newtons) ;
-    end
+    counter = 1;
+    snapshots_saved = 1;
+    printf("[*] Computing simulation...\n");
 
-    function [Fg] = compute_gravitational(rocket_CoM)
-        g = -1 * G * M_t * (rocket_CoM / (norm(rocket_CoM)^3));
-        Fg = rocket_mass * g;
-    end
-
-    function [Fvis] = compute_friction(v, r)
-        A = (pi * rocket_radius^2 * cos(alpha)) + (2 * rocket_radius * rocket_height * sin(alpha));
-        rho = rho_0 * exp(1)^((earth_radius - norm(r)) / h_0);
-        Fvis = -1 * 1/2 * A * rho * C_vis * norm(v) * v;
-    end
-
-    function [a] = compute_acceleration(theta, velocity, rocket_CoM)
-        Fp = compute_propulsion(theta);
-        Fg = compute_gravitational(rocket_CoM);
-        Fvis = compute_friction(velocity, rocket_CoM);
-        F = Fp + Fg + Fvis;
-        a = F / rocket_mass;
-    end
-
-    function [g] = compute_g(q, t)
-        velocity = [q(1); q(2); q(3)];
-        rocket_CoM = [q(4); q(5); q(6)];
-        a = compute_acceleration(theta, velocity, rocket_CoM);
-        g = [a(1); a(2); a(3); velocity(1); velocity(2); velocity(3)];
-    end
-
-    function [m] = compute_rocket_mass_at_time(t) 
-      m = 320 * 1000 - 1200 * t;
-    end
-
-    function [MI] = compute_moment_of_inertia(t) 
-      rocket_mass = compute_rocket_mass_at_time(t);
-      I_zz = rocket_mass/2 * rocket_radius^2;
-      I_xx = I_yy = (rocket_mass/4 * rocket_radius^2)+(rocket_mass/12 * rocket_height^2);
-      I = [
-        I_xx, 0, 0;
-        0, I_yy, 0;
-        0, 0, I_zz;
-      ];
-      MI = R*I*transpose(R);
-    end
-
-    dT = 0.1; % Set delta t to an arbitrairy 0.01 seconds.
-    current_q = [0; 0; 0; x(1); y(1); z(1); ];
-    current_t = 0;
-    previous_q = [0; 0; 0; 0; 0; 0];
-    previous_t = 0;
-    current_rocket_CoM = [x(1); y(1); z(1)];
-    counter = 0;
-    printf("Computing simulation...");
-
-    while !verify_completion(counter, current_rocket_CoM)
-        velocity_vector = [previous_q(1); previous_q(2); previous_q(3)];
-        if (norm(velocity_vector))
-            alpha = asin(previous_q(3) / norm(velocity_vector));
-        else
-            alpha = 0;
+    while !completed
+        % Update return arrays:
+        time = time + dT;
+        g = @compute_g;
+        q = SEDRK4t0(q, time, dT, g);
+        counter = counter + 1;
+        r = [0; q(3); q(4)];
+        if(abs(time - time_since_last_snapshot) >= snapshot_timer)
+            snapshots_saved = snapshots_saved + 1;
+            time_since_last_snapshot = time;
+            t = [t; time];
+            x = [x; r(1)];
+            y = [y; r(2)];
+            z = [z; r(3)];
         end
 
-        previous_q = current_q;
-        previous_t = current_t;
-        counter = counter + 1;
-        k1 = compute_g(previous_q, previous_t);
-        k2 = compute_g(previous_q + (dT / 2 * k1), previous_t + dT / 2);
-        k3 = compute_g(previous_q + (dT / 2 * k2), previous_t + dT / 2);
-        k4 = compute_g(previous_q + (dT * k3), previous_t + dT / 2);
-        current_q = previous_q + (dT * (k1 + (2 * k2) + (2 * k3) + k4)) / 6;
-        current_rocket_CoM = [current_q(4), current_q(5), current_q(6)];
+        if (snapshots_saved >= 1000)
+            printf("[!] Reached above 1000 snapshots")
+            break;
+        end
 
-        % Update values:
-        x = [x; current_rocket_CoM(1)];
-        y = [y; current_rocket_CoM(2)];
-        z = [z; current_rocket_CoM(3)];
-        t = [t; current_t];
-        current_t = current_t + dT;
-        masse_totale = compute_rocket_mass_at_time(t(end));
-        % if (masse_totale <= rocket_mass - fuel_mass)
-        % end
+        completed = verify_completion([0; q(3); q(4)]);
     end
 
-    if (counter < 100)
-        printf("Number of increments does not reach past 100...");
-    elseif (counter > 1000)
-        printf("Number of increments is above 1000...");
+    if (counter <= 100)
+        printf("[!] Change deltaT, because snapshot count is too low...\n")
     end
 
-    Vf = [current_q(1); current_q(2); current_q(3)];
-
+    printf("[*] We got %i iterations\n", counter)
+    printf("[*] We got %i snapshots\n", snapshots_saved)
+    Vf = [v0(1); q(2); q(3)];
 end
--- Devoir2/RouleDevoir2.m
+++ Devoir2/RouleDevoir2.m
@@ -9,12 +9,12 @@ axis equal;
   hold;
   xlabel('y(m)')
   ylabel('z(m)')
-% Valeur de thêta à modifier
-  theta=0.0;
+% Valeur de thï¿½ta ï¿½ modifier
+  theta=pi/2;
 [Vf t x y z]=Devoir2(theta);
   sz=size(t,1);
   plot(y,z,'r')
- fprintf('Angle thêtan          %10.9f rad \n',theta);
+ fprintf('Angle thï¿½tan          %10.9f rad \n',theta);
  fprintf('Temps final          %10.6f s \n',t(sz));
  fprintf('Vitesse finale          %10.6f m/s \n',Vf);
  fprintf('Position finale        ( %10.0f   , %10.0f   ,%10.0f )m \n',x(sz),y(sz),z(sz));
